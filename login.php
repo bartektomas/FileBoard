d7c060822f1848184cabffae9878d01294b07c47
Jeremy Bost <bostjs1@gcc.edu>
2016-11-12 15:18:58 -0500

    Basic login functionality added
    Adds session info if user and password match. No hashing as of now. Someone needs to style login page too.

--- login.php
+++ login.php
@@ -6,17 +6,48 @@
 
             Can probably draw from some PHP login page example for most of this.
 */
+
+session_start();
+
+// start database connection with PDO object $conn
+require_once 'database.php';
+
+// check if GET logout=true
+if (isset($_GET['logout']) && $_GET['logout']) {
+    session_destroy();
+}
+// check login info if posted
+elseif (isset($_POST['login']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+    foreach ($conn->query("SELECT * FROM users") as $row) {
+        if ($row['email'] === $_POST['email'] && $row['password'] === $_POST['password']) {
+            $_SESSION['valid'] = true;
+            $_SESSION['userid'] = $row['userid'];
+            break;
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Fileboard</title>
+        <title>Login - Fileboard</title>
     </head>
     <body>
-        <?php
-
-        ?>
+        <?php print_r($_POST); ?>
+        <div class="container">
+            <form class = "form-signin" role = "form" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "post">
+                <h4 class = "form-signin-heading"><?php echo ''; ?></h4>
+                <input type = "text" class = "form-control"
+                    name = "email" placeholder = "test@test.com"
+                    required autofocus></br>
+                <input type = "password" class = "form-control"
+                    name = "password" placeholder = "pass" required>
+                <button class = "btn btn-lg btn-primary btn-block" type = "submit"
+                    name = "login">Login</button>
+           </form>
+        </div>
     </body>
 </html>

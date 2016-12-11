5aef71c210b60141eddc7f9a03ba9968db16a16f
Jeremy Bost <bostjs1@gcc.edu>
2016-12-11 14:40:30 -0500

    Help guide looks nice, thanks. Touched it up a bit

--- help.php
+++ help.php
@@ -1,133 +1,149 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Help</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="login_style.css">
-    </head>
-    <body>
-    <h1>
-        How to use Fileboard
-    </h1>
-
-    <h2>Contents</h2>
-        <ul>
-            <a href = "#drawing-heading"><li>Drawing on the board</li></a>
-            <a href = "#adding-text-heading"><li>Adding text to the board</li></a>
-            <a href = "#adding-shape-heading"><li>Adding a shape to the board</li></a>
-        </ul>
-
-
-    <h2 id = "drawing-heading">
-        Drawing on the board:
-    </h2>
-    <p>
-    Step 1:
-    Click on the pencil button.
-    </p>
-    <div class="row">
-        <div class="col-xs-1">
-        </div>
-        <div class="col-xs-10">
-            <img src = "Images/select pencil.jpg" alt = "Selecting the pencil" class = "img-responsive center-block">
-        </div>
-        <div class="col-xs-1">
-        </div>
-    </div>
-    <div>
-    Step 2:
-    Click and drag anywhere on the canvas to draw.
-    </div>
-    <div class="row">
-        <div class="col-xs-1">
-        </div>
-        <div class="col-xs-10">
-            <img src = "Images/draw2.png" alt = "drawing" class = "img-responsive center-block">
-        </div>
-        <div class="col-xs-1">
-        </div>
-    </div>
-    <h2 id = "adding-text-heading">
-        Adding text to the board:
-    </h2>
-    <div>
-    Step 1:
-    Click on the text button.
-    </div>
-    <div class="row">
-        <div class="col-xs-1">
-        </div>
-        <div class="col-xs-10">
-            <img src = "Images/text 1.jpg" alt = "Selecting text button" class = "img-responsive center-block">
+<head>
+    <meta charset="utf-8">
+    <title>Help - FileBoard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="login_style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-10">
+                <div id="go-back">
+                    <a href=".">Back to FileBoard</a>
                 </div>
-        <div class="col-xs-1">
-        </div>
-    </div>
-    Step 2:
-    Click anywhere on the canvas
-    </div>
-    <div class="row">
-        <div class="col-xs-1">
-        </div>
-        <div class="col-xs-10">
-            <img src = "Images/text 2.jpg" alt = "cursor positioned" class = "img-responsive center-block">
-        </div>
-        <div class="col-xs-1">
-        </div>
-    </div>
-    Step 3:
-    Type
-    </div>
-    <div class="row">
-        <div class="col-xs-1">
-        </div>
-        <div class="col-xs-10">
-            <img src = "Images/text 3.jpg" alt = "typing" class = "img-responsive center-block">
-        </div>
-        <div class="col-xs-1">
-        </div>
-    </div>
-    <h2 id = "adding-shape-heading">
-        Adding a shape to the board:
-    </h2>
-    <p>
-    Step 1:
-    Click on the shapes button.
-    </p>
-    </div>
-    <div class="row">
-        <div class="col-xs-1">
-        </div>
-        <div class="col-xs-10">
-            <img src = "Images/shape 1.jpg" alt = "Selecting text button" class = "img-responsive center-block">
+                <h1>
+                    How to use Fileboard
+                </h1>
+
+                <h2>Contents</h2>
+                <ul>
+                    <a href = "#drawing-heading"><li>Drawing on the board</li></a>
+                    <a href = "#adding-text-heading"><li>Adding text to the board</li></a>
+                    <a href = "#adding-shape-heading"><li>Adding a shape to the board</li></a>
+                </ul>
+
+
+                <h2 id = "drawing-heading">
+                    Drawing on the board:
+                </h2>
+                <p>
+                    Step 1:
+                    Click on the pencil button.
+                </p>
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class="col-xs-10">
+                        <img src = "Images/select pencil.jpg" alt = "Selecting the pencil" class = "img-responsive center-block">
+                    </div>
+                    <div class="col-xs-1">
+                    </div>
                 </div>
-        <div class="col-xs-1">
+                <div>
+                    Step 2:
+                    Click and drag anywhere on the canvas to draw.
+                </div>
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class="col-xs-10">
+                        <img src = "Images/draw2.png" alt = "drawing" class = "img-responsive center-block">
+                    </div>
+                    <div class="col-xs-1">
+                    </div>
+                </div>
+                <h2 id = "adding-text-heading">
+                    Adding text to the board:
+                </h2>
+                <div>
+                    Step 1:
+                    Click on the text button.
+                </div>
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class="col-xs-10">
+                        <img src = "Images/text 1.jpg" alt = "Selecting text button" class = "img-responsive center-block">
+                    </div>
+                    <div class="col-xs-1">
+                    </div>
+                </div>
+                <p>
+                    Step 2:
+                    Click anywhere on the canvas
+                </p>
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class="col-xs-10">
+                        <img src = "Images/text 2.jpg" alt = "cursor positioned" class = "img-responsive center-block">
+                    </div>
+                    <div class="col-xs-1">
+                    </div>
+                </div>
+                <p>
+                    Step 3:
+                    Type
+                </p>
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class="col-xs-10">
+                        <img src = "Images/text 3.jpg" alt = "typing" class = "img-responsive center-block">
+                    </div>
+                    <div class="col-xs-1">
+                    </div>
+                </div>
+                <h2 id = "adding-shape-heading">
+                    Adding a shape to the board:
+                </h2>
+                <p>
+                    Step 1:
+                    Click on the shapes button.
+                </p>
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class="col-xs-10">
+                        <img src = "Images/shape 1.jpg" alt = "Selecting text button" class = "img-responsive center-block">
+                    </div>
+                    <div class="col-xs-1">
+                    </div>
+                </div>
+                <p>
+                    Step 2:
+                    Select shape type
+                </p>
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class="col-xs-10">
+                        <img src = "Images/shape 2.jpg" alt = "cursor positioned" class = "img-responsive center-block">
+                    </div>
+                    <div class="col-xs-1">
+                    </div>
+                </div>
+                <p>
+                    Step 3:
+                    Click anywhere on the canvas to add the shape
+                </p>
+                <div class="row">
+                    <div class="col-xs-1">
+                    </div>
+                    <div class="col-xs-10">
+                        <img src = "Images/shape 3.jpg" alt = "typing" class = "img-responsive center-block">
+                    </div>
+                    <div class="col-xs-1">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-1">
+            </div>
         </div>
     </div>
-    Step 2:
-    Select shape type
-    </div>
-    <div class="row">
-        <div class="col-xs-1">
-        </div>
-        <div class="col-xs-10">
-            <img src = "Images/shape 2.jpg" alt = "cursor positioned" class = "img-responsive center-block">
-        </div>
-        <div class="col-xs-1">
-        </div>
-    </div>
-    Step 3:
-    Click anywhere on the canvas to add the shape
-    </div>
-    <div class="row">
-        <div class="col-xs-1">
-        </div>
-        <div class="col-xs-10">
-            <img src = "Images/shape 3.jpg" alt = "typing" class = "img-responsive center-block">
-        </div>
-        <div class="col-xs-1">
-        </div>
-    </div>
-    </body>
+</body>
 </html>
